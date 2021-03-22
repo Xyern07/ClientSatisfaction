e391d2c6060750d79951e3b0a0546e17083ae666
Xyern07 <xyernkc@gmail.com>
2021-03-22 23:03:42 +0800

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
 
         }
 
@@ -41,6 +42,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
